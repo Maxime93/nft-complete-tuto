d2dac207c8ffaa293ba77a9fb6246269188cc18f
Maxime Richard <maxrichard@apple.com>
2021-09-01 10:10:03 -0700

    forst-commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# https://www.youtube.com/watch?v=p36tXHX1JD8
+
+
+
+## Deploy an NFT from scratch
---
+++ contracts/AdvancedCollectible.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ER721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
